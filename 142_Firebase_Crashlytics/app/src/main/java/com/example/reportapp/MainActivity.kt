8ab44e7da526dd7a2cc3d6512f91881995a44088
Dhino12 <ucihadhino@gmail.com>
2021-02-08 17:36:07 +0700

    report crash to firebase with crashlytics

--- 142_Firebase_Crashlytics/app/src/main/java/com/example/reportapp/MainActivity.kt
+++ 142_Firebase_Crashlytics/app/src/main/java/com/example/reportapp/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.reportapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnCrash = findViewById<Button>(R.id.btn_crash)
+        btnCrash.setOnClickListener {
+            // Kode di atas digunakan untuk membuat
+            // log/catatan yang dapat Anda selipkan pada aplikasi Anda,
+            FirebaseCrashlytics.getInstance().log("Clicked on Button")
+
+            // Mengirim data ke report
+            FirebaseCrashlytics.getInstance().setCustomKey("str_key", "some_data")
+            throw java.lang.RuntimeException("Test Crash")
+        }
     }
 }
