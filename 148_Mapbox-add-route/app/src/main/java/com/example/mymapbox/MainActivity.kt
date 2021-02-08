a150fa21e6f145580fc3320fc74b54ea4fe19ff3
Dhino12 <ucihadhino@gmail.com>
2021-02-08 21:46:33 +0700

    Final add route and Navigation walk

---
+++ 148_Mapbox-add-route/app/src/main/java/com/example/mymapbox/MainActivity.kt
@@ -0,0 +1,247 @@
+package com.example.mymapbox
+
+import android.annotation.SuppressLint
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import android.view.animation.BounceInterpolator
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.api.directions.v5.models.DirectionsResponse
+import com.mapbox.api.directions.v5.models.DirectionsRoute
+import com.mapbox.geojson.Point
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.location.LocationComponent
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
+import com.mapbox.mapboxsdk.location.LocationComponentOptions
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.location.modes.RenderMode
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions
+import com.mapbox.services.android.navigation.ui.v5.NavigationLauncher
+import com.mapbox.services.android.navigation.ui.v5.NavigationLauncherOptions
+import com.mapbox.services.android.navigation.ui.v5.route.NavigationMapRoute
+import com.mapbox.services.android.navigation.v5.navigation.NavigationRoute
+import retrofit2.Call
+import retrofit2.Response
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mapView:MapView
+    private lateinit var btnNavigation:Button
+    private lateinit var mapBoxMap:MapboxMap
+    private lateinit var symbolManager:SymbolManager
+    private lateinit var locationComponent:LocationComponent
+    private lateinit var myLocation:LatLng
+    private lateinit var permissionManager:PermissionsManager
+    private lateinit var navigationRoute:NavigationMapRoute
+    private var currentRoute:DirectionsRoute? = null
+
+    companion object{
+        private const val ICON_ID = "ICON_ID"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mapView = findViewById(R.id.mapView)
+        btnNavigation = findViewById(R.id.btnNavigation)
+
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync {
+            mapBoxMap -> this.mapBoxMap = mapBoxMap
+            mapBoxMap.setStyle(Style.MAPBOX_STREETS){
+                style ->
+                // Get Marker
+                symbolManager = SymbolManager(mapView, mapBoxMap, style)
+                symbolManager.iconAllowOverlap = true
+
+                style.addImage( ICON_ID,BitmapFactory.decodeResource(resources, R.drawable.mapbox_marker_icon_default) )
+
+                navigationRoute = NavigationMapRoute(
+                    null,
+                    mapView,
+                    mapBoxMap,
+                    R.style.NavigationMapRoute
+                )
+
+                showMarkerLocation()
+                showMyLocation(style)
+                addMarkerOnClick()
+                showNavigation()
+            }
+        }
+    }
+
+    private fun showMarkerLocation(){
+        val showMarker = LatLng(-8.0459557,110.6441672)
+        symbolManager.create(
+            SymbolOptions()
+                .withLatLng(LatLng(showMarker.latitude, showMarker.longitude))
+                .withIconImage(ICON_ID)
+                .withIconSize(1.5f)
+                .withIconOffset(arrayOf(0f, -1.5f))
+                .withTextField("Goa Jombang")
+                .withTextHaloColor("rgba(255, 255, 255, 100)")
+                .withTextHaloWidth(5.0f)
+                .withTextAnchor("top")
+                .withTextOffset(arrayOf(0f,1.5f))
+                .withDraggable(true)
+        )
+        mapBoxMap.moveCamera(CameraUpdateFactory.newLatLngZoom(showMarker, 10.0))
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun showMyLocation(style: Style) {
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            val locationComponentOptions = LocationComponentOptions.builder(this)
+                .pulseEnabled(true)
+                .pulseColor(Color.BLUE)
+                .pulseAlpha(.4f)
+                .pulseInterpolator(BounceInterpolator())
+                .build()
+            val locationComponentActivationOptions = LocationComponentActivationOptions
+                .builder(this, style)
+                .locationComponentOptions(locationComponentOptions)
+                .build()
+            locationComponent = mapBoxMap.locationComponent
+            locationComponent.activateLocationComponent(locationComponentActivationOptions)
+            locationComponent.isLocationComponentEnabled = true
+            locationComponent.cameraMode = CameraMode.TRACKING
+            locationComponent.renderMode = RenderMode.COMPASS
+
+            myLocation = LatLng(locationComponent.lastKnownLocation?.latitude as Double, locationComponent.lastKnownLocation?.longitude as Double)
+            mapBoxMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 12.0))
+        } else {
+            permissionManager = PermissionsManager(object : PermissionsListener{
+                override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+                    Toast.makeText(this@MainActivity, "Anda harus mengizinkan location permission untuk menggunakan aplikasi ini", Toast.LENGTH_SHORT).show()
+                }
+                override fun onPermissionResult(granted: Boolean) {
+                    if (granted) {
+                        mapBoxMap.getStyle { style ->
+                            showMyLocation(style)
+                        }
+                    } else {
+                        finish()
+                    }
+                }
+            })
+            permissionManager.requestLocationPermissions(this)
+        }
+    }
+
+    private fun addMarkerOnClick(){
+        mapBoxMap.addOnMapClickListener {
+            point -> symbolManager.deleteAll()
+            symbolManager.create(
+                SymbolOptions()
+                    .withLatLng(LatLng(point.latitude, point.longitude))
+                    .withIconImage(ICON_ID)
+                    .withDraggable(true)
+            )
+            val destination = Point.fromLngLat(point.longitude, point.latitude)
+            val origin = Point.fromLngLat(myLocation.longitude, myLocation.latitude)
+            requestRoute(origin, destination)
+            btnNavigation.visibility = View.VISIBLE
+            true
+        }
+    }
+
+    private fun requestRoute(origin:Point, destination:Point){
+        navigationRoute.updateRouteVisibilityTo(false)
+        NavigationRoute.builder(this)
+            .accessToken(getString(R.string.access_token_api))
+            .origin(origin)
+            .destination(destination)
+            .build()
+            .getRoute(object : retrofit2.Callback<DirectionsResponse>{
+                override fun onResponse(
+                    call: Call<DirectionsResponse>,
+                    response: Response<DirectionsResponse>
+                ) {
+                    if(response.body() == null){
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Rute tidak ditemukan, pastikan Anda menyetel pengguna yang tepat dan token akses.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        return
+                    }else if(response.body()?.routes()?.size == 0){
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Rute tidak ditemukan.", Toast.LENGTH_SHORT)
+                            .show()
+                        return
+                    }
+                    currentRoute = response.body()?.routes()?.get(0)
+                    navigationRoute.addRoute(currentRoute)
+                }
+
+                override fun onFailure(call: Call<DirectionsResponse>, t: Throwable) {
+                    Toast.makeText(this@MainActivity, "Error : $t", Toast.LENGTH_SHORT).show()
+                }
+
+            })
+    }
+
+    private fun showNavigation(){
+        btnNavigation.setOnClickListener{
+            val simulateRoute = true
+
+            if(currentRoute != null){
+                val option = NavigationLauncherOptions.builder()
+                    .directionsRoute(currentRoute)
+                    .shouldSimulateRoute(simulateRoute)
+                    .build()
+                NavigationLauncher.startNavigation(this, option)
+            }else{
+                Toast.makeText(this, "currentRoute NULL",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+}
